d02435b63dd29275dcef81b87c8785ebd091e3bd
Pihro <fluffyanddeadly999@gmail.com>
2013-01-09 10:21:39 -0600

    Fixed coordinate issues

--- scripts/maps/chishiki_no_sabaku.lua
+++ scripts/maps/chishiki_no_sabaku.lua
@@ -2,6 +2,6 @@ require "scripts/npcs"
 require "scripts/npcs/chishiki_no_sabaku/testnpc"
 
 atinit(function()
-    npc_create("testnpc", npc.testnpc, GENDER_MALE, 2760 * TILESIZE,
-               3456 * TILESIZE, testnpc_talk, testnpc_update)
+    npc_create("testnpc", npc.testnpc, GENDER_MALE, 90 * TILESIZE + TILESIZE /2,
+               112 * TILESIZE + TILESIZE / 2, testnpc_talk, testnpc_update)
 end)
