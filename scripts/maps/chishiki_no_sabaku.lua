f7b5f0f71077dd20d160e4df6c6a24f4f38d9abb
Pihro <fluffyanddeadly999@gmail.com>
2013-01-09 10:04:18 -0600

    Npc Mod

--- scripts/maps/chishiki_no_sabaku.lua
+++ scripts/maps/chishiki_no_sabaku.lua
@@ -2,6 +2,6 @@ require "scripts/npcs"
 require "scripts/npcs/chishiki_no_sabaku/testnpc"
 
 atinit(function()
-    npc_create("testnpc", npc.testnpc, GENDER_MALE, 122 * TILESIZE,
-               108 * TILESIZE, testnpc_talk, testnpc_update)
+    npc_create("testnpc", npc.testnpc, GENDER_MALE, 2760 * TILESIZE,
+               3456 * TILESIZE, testnpc_talk, testnpc_update)
 end)
