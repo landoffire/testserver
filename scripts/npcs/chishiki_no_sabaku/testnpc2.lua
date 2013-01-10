d33338527c1559a327850aa6df22a8c60226c29f
Pihro <fluffyanddeadly999@gmail.com>
2013-01-09 21:09:27 -0600

    Tried to fix lua npc

--- scripts/npcs/chishiki_no_sabaku/testnpc2.lua
+++ scripts/npcs/chishiki_no_sabaku/testnpc2.lua
@@ -1,9 +1,10 @@
-function testnpc2_update(npc)
-    npclib.walkaround_wide(npc)
-end
-
 function testnpc2_talk(npc, ch)
     npc_say(npc, ch, "Good day!",
             "This is Pihro's first self written lua script."
             .. "His learning of manaserv is increasing in volume.")
 end
+
+function testnpc2_update(npc)
+    npclib.walkaround_wide(npc)
+end
+
