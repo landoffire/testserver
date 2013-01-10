e961586b20cb6232dc0b107b1827a7b4cda20c9d
Pihro <fluffyanddeadly999@gmail.com>
2013-01-09 21:11:39 -0600

    Fixed the npc issue

--- scripts/maps/chishiki_no_sabaku.lua
+++ scripts/maps/chishiki_no_sabaku.lua
@@ -5,6 +5,7 @@ require "scripts/npcs/chishiki_no_sabaku/testnpc2"
 atinit(function()
     npc_create("testnpc", npc.testnpc, GENDER_MALE, 90 * TILESIZE + TILESIZE /2,
                112 * TILESIZE + TILESIZE / 2, testnpc_talk, testnpc_update)
+end)
 
 atinit(function()
     npc_create("testnpc2", npc.testnpc2, GENDER_MALE, 90 * TILESIZE + TILESIZE /2,
