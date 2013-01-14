179f319f8d2cc30b9c08eb8d63d0b2bb8c17a75c
PyroSamurai <vikingdude394@yahoo.com>
2013-01-13 21:48:40 -0500

    Adding files.

---
+++ tilesets/table-sets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="table-sets" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/table-sets.png" width="512" height="512"/>
+</tileset>
